2187e911276c25b1fadea9c706a9e89c12e1e60c
Aditya Sharma <sharmaaditya570191@gmail.com>
2020-03-11 02:09:28 +0530

    Migrate project to Create-React-App toolchain

---
+++ src/components/Heading.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router'
+import Logo from '../assets/img/logo.svg'
+
+const Heading = () => {
+  return (
+    <div id='heading'>
+      <div id='logo'>
+        <Link to='/'>
+          <img id='logo-img' className='button' src={Logo} alt='logo' />
+        </Link>
+      </div>
+      <div id='title'>
+        <Link to='/'>
+          <span className='button'>Boss Machine</span>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Heading
---
+++ src/components/Home.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router'
+
+import { cancelMeetingsThunk } from '../store/meetings'
+
+import AllMeetings from './AllMeetings'
+import minionIcon from '../assets/img/minion_icon_home.svg'
+import minionMoney from '../assets/img/minion_icon_money.svg'
+
+const Home = ({ cancelMeetings }) => {
+  return (
+    <div id='landing-page'>
+      <div id='launch-buttons'>
+        <Link to='/minions'>
+          <div id='minions-launch' className='button launch-button'>
+            <img className='button launch-icon' src={minionIcon} alt='' />
+            <div className='button label launch-label'>MINIONS.exe</div>
+          </div>
+        </Link>
+        <Link to='/ideas'>
+          <div id='ideas-launch' className='button launch-button'>
+            <img className='button launch-icon' src={minionMoney} alt='' />
+            <div className='button label launch-label'>MILLION $ IDEAS.exe</div>
+          </div>
+        </Link>
+      </div>
+      <AllMeetings />
+      <div id='meetings-cancel' className='button' onClick={cancelMeetings}>
+        Cancel All
+      </div>
+    </div>
+  )
+}
+
+const mapDispatch = dispatch => ({
+  cancelMeetings: () => {
+    dispatch(cancelMeetingsThunk())
+  }
+})
+
+export default connect(null, mapDispatch)(Home)
