ff9c1867560dc80c8ff0a939257e0d76f689be36
James Kaviyil Jose <jamesjose03@gmail.com>
2020-04-16 18:13:58 +0530

    add first page component

---
+++ src/components/FirstContent.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+function firstContent() {
+    return (
+        <div>
+            <div className="row wrapper">
+                <div className="col-sm-4">
+                    <div className="card">
+                        <div className="card-body">
+                            <label className="container">
+                                <input type="checkbox" checked="checked" />
+                                <span className="checkmark one"></span>
+                            </label>
+                            <h5 className="card-title">Market Analysis</h5>
+                            <p className="card-text">lorem ipsum</p>
+                        </div>
+                    </div>
+                </div>
+                    <div className="col-sm-4">
+                        <div className="card">
+                            <div className="card-body">
+                                <label className="container">
+                                    <input type="checkbox" checked="checked" />
+                                    <span className="checkmark two"></span>
+                                </label>
+                                <h5 className="card-title">Financial Advice</h5>
+                                <p className="card-text">lorem ipsum</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-4">
+                        <div className="card">
+                            <div className="card-body">
+                                <label className="container">
+                                    <input type="checkbox" checked="checked" />
+                                    <span className="checkmark three"></span>
+                                </label>
+                                <h5 className="card-title">Web Solution</h5>
+                                <p className="card-text">lorem ipsum</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </div>
+    );
+}
+
+export default firstContent;
