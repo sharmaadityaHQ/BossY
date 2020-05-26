e4877dbbdf161cb59cd4ef69441685dd460cbf20
satyam3976 <chaudhary.satyam3976@gmail.com>
2020-05-27 01:31:46 +0530

    Logo changed and enhancements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,12 @@
 import React from 'react'
 import '../assets/scss/footer.scss';
+import Logo from '../assets/img/twitter.svg';
+import Logo2 from '../assets/img/google.png';
+import Logo3 from '../assets/img/facebook.svg';
+import Gps from '../assets/img/gps.svg';
+import Pin from '../assets/img/pin.svg';
+import Email from '../assets/img/email.svg'
+import Telephone from '../assets/img/telephone.svg'
 
 const Footer = () => {
     return (
@@ -12,27 +19,27 @@ const Footer = () => {
                     the 1500s, 
                 </p>
                 <div className="footer-section1-images">
-                    <img alt="twitter" src="../assets/img/twitter.svg"></img>
-                    <img alt="facebook" src="../assets/img/facebook.svg"></img>
-                    <img alt="google" src="../assets/img/google.svg"></img>
+                    <img alt="twitter" src={Logo}></img>
+                    <img alt="facebook" src={Logo3}></img>
+                    <img alt="google" src={Logo2}></img>
                 </div>
             </div>
             <div className="footer-section2">
                 <h3>Contact Info</h3>
                 <div className="footer-section2-item1">
-                    <img alt="map"/>
+                    <img alt="map" src={Gps}/>
                     <p>113 Futton street, Suite</p>
                 </div>
                 <div className="footer-section2-item2">
-                    <img alt="pin"></img>
+                    <img alt="pin" src={Pin}></img>
                     <p>New York NY</p>
                 </div>
                 <div className="footer-section2-item3">
-                    <img alt="message"></img>
+                    <img alt="message" src={Email}></img>
                     <p>youremail@yourdomain.com</p>
                 </div>
                 <div className="footer-section2-item4">
-                    <img alt="phone"></img>
+                    <img alt="phone" src={Telephone}></img>
                     <p>+88 (0) 101 0000</p>
                 </div>
             </div>
@@ -41,7 +48,7 @@ const Footer = () => {
                     <p>If you want to change your wordpress</p>
                 </div>
                 <div className="footer-section3-content">
-                    <img twitter></img>
+                    <img src={Logo}></img>
                     <div>
                     <h3>Qade Interactive</h3>
                     <h5>4 DAYS AGO</h5>
@@ -51,9 +58,7 @@ const Footer = () => {
             <div className="footer-section4">
                 <h3>Support and Download</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur 
-                    adipiscing elit, sed do eiusmod tempor incididunt ut 
-                    labore et dolore magna aliqua. Odio eu feugiat pretium 
-                    nibh ipsum consequat. 
+                    adipiscing elit, sed do eiusmod tempor 
                 </p>
                 <div className="footer-section4-button">
                 <button>App Store</button>
