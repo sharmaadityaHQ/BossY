f2afb30d73f14c47a63fed1cbeaf4982c2ea814a
Aditya Sharma <sharmaaditya570191@gmail.com>
2020-05-31 16:34:46 +0530

    Revert breaking changes

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -3,8 +3,18 @@ import { Link } from '@reach/router'
 import Logo from '../assets/img/logo.svg'
 
 const Heading = () => {
+  const changeMode = () => {
+    // TODO: change the mode between day and night
+    console.log("mode changed");
+  }
   return (
     <div id='heading'>
+      <div className="change-theme">
+        <label className="switch">
+          <input type="checkbox" onClick={changeMode} />
+            <span className="slider round"></span>
+        </label>
+      </div>
       <div id='logo'>
         <Link to='/'>
           <img id='logo-img' className='button' src={Logo} alt='logo' />
